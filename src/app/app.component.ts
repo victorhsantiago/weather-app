0bce5a36a5cfa6d77e2074cc4112ac272973d506
Victor Henrique Santiago <victorhsantiago3@gmail.com>
2018-09-15 22:48:11 -0300

    it's working!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,48 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { UiService } from "./services/ui/ui.service";
 import { WeatherService } from "./services/weather/weather.service";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"]
 })
+
 export class AppComponent {
-  constructor(public ui: UiService, public weather: WeatherService) {}
+	constructor(public ui: UiService, public weather: WeatherService) { }
 
-  title = "weather-app";
-  darkModeActive: boolean;
-  showMenu = false;
+	title = "weather-app";
+	darkModeActive: boolean;
+	showMenu = false;
+	city;
+	@ViewChild('cityInput') cityInput;
 
-  ngOnInit() {
-    this.ui.darkModeState.subscribe(value => {
-      this.darkModeActive = value;
-    });
-  }
+	ngOnInit() {
+		this.ui.darkModeState.subscribe(value => {
+			this.darkModeActive = value;
+		});
+	}
 
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
-  }
+	// ngAfterViewInit() {
+	//   debugger
+	//   this.cityInput.valueChanges
+	//     .debounceTime(500) //before emitting last event
+	//     .distinctUntilChanged()
+	//     .subscribe((value) => {
+	//       console.log('delayed key press value', value);
+	//       this.city = value
+	//     });
+	// }
 
-  modeToggleSwitch() {
-    this.ui.darkModeState.next(!this.darkModeActive);
-  }
+	toggleMenu() {
+		this.showMenu = !this.showMenu;
+	}
 
-  submitSearch(city: string) {
-    this.weather.getForecast(city);
-    this.weather.getCityWeatherByName(city);
-    console.log(this.weather.getCurrenteTemp(city));
-  }
+	modeToggleSwitch() {
+		this.ui.darkModeState.next(!this.darkModeActive);
+	}
+
+	submitSearch(city: string) {
+		this.city = city;
+	}
 }
